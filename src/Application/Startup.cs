e5b980b26e774fc4fcdce6c37d48b8276ad8b070
NguyenVanVuong <nguyenvanvuong@vnpt.vn>
2026-04-14 16:41:11 +0700

    feat: initialize Application layer with MediatR, FluentValidation, and project dependencies

---
+++ src/Application/Startup.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Boilerate.Application;
+
+public static class Startup
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        
+        return services
+            .AddValidatorsFromAssembly(assembly)
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+    }
+}
